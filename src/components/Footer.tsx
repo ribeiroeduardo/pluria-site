c8cb175138bf27afc3d020051abf3eadd1be6d0b
Eduardo Ribeiro <edunius@gmail.com>
2025-03-03 14:06:36 -0300

    Updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,85 +3,28 @@ import { cn } from '@/lib/utils';
 import { Facebook, Instagram, Twitter, Youtube } from 'lucide-react';
 const Footer = () => {
   const currentYear = new Date().getFullYear();
-  const footerLinks = [{
-    title: 'Company',
-    links: [{
-      label: 'About Us',
-      href: '#about'
-    }, {
-      label: 'Our Process',
-      href: '#process'
-    }, {
-      label: 'Portfolio',
-      href: '#portfolio'
-    }, {
-      label: 'Contact',
-      href: '#contact'
-    }]
-  }, {
-    title: 'Services',
-    links: [{
-      label: 'Custom Guitars',
-      href: '#'
-    }, {
-      label: 'Repair & Maintenance',
-      href: '#'
-    }, {
-      label: 'Restoration',
-      href: '#'
-    }, {
-      label: 'Consulting',
-      href: '#'
-    }]
-  }, {
-    title: 'Resources',
-    links: [{
-      label: 'Blog',
-      href: '#'
-    }, {
-      label: 'FAQ',
-      href: '#'
-    }, {
-      label: 'Testimonials',
-      href: '#testimonials'
-    }, {
-      label: 'Care Guide',
-      href: '#'
-    }]
-  }, {
-    title: 'Legal',
-    links: [{
-      label: 'Privacy Policy',
-      href: '#'
-    }, {
-      label: 'Terms of Service',
-      href: '#'
-    }, {
-      label: 'Warranty',
-      href: '#'
-    }, {
-      label: 'Shipping Policy',
-      href: '#'
-    }]
-  }];
-  const socialLinks = [{
-    icon: Facebook,
-    href: '#',
-    label: 'Facebook'
-  }, {
-    icon: Instagram,
-    href: '#',
-    label: 'Instagram'
-  }, {
-    icon: Twitter,
-    href: '#',
-    label: 'Twitter'
-  }, {
-    icon: Youtube,
-    href: '#',
-    label: 'YouTube'
-  }];
-  return <footer className="bg-primary text-white pt-12 pb-6">
+
+  const footerLinks = [
+    {
+      title: 'Legal',
+      links: [
+        { label: 'Privacy Policy', href: '#' },
+        { label: 'Terms of Service', href: '#' },
+        { label: 'Warranty', href: '#' },
+        { label: 'Shipping Policy', href: '#' },
+      ],
+    },
+  ];
+
+  const socialLinks = [
+    { icon: Facebook, href: '#', label: 'Facebook' },
+    { icon: Instagram, href: '#', label: 'Instagram' },
+    { icon: Twitter, href: '#', label: 'Twitter' },
+    { icon: Youtube, href: '#', label: 'YouTube' },
+  ];
+
+  return (
+    <footer className="bg-primary text-white pt-12 pb-6">
       <div className="container mx-auto px-4 md:px-6">
         <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-8 mb-12">
           <div className="col-span-2">
